c0e1fda9a0dd2b9de51ae752e10688af2d122c14
Shawn Bataju <bshawngreen@gmail.com>
2022-09-24 11:52:46 -0500

    Added pdfs for elevator pitch and research assignments

---
+++ Week3 Labs/src/arrays_and_methods/ArraysAndMethods.java
@@ -0,0 +1,32 @@
+package arrays_and_methods;
+
+import java.lang.reflect.Array;
+
+public class ArraysAndMethods {
+
+	public static void main(String[] args) {
+		int[] numbers = new int[6];
+		numbers[0] = 1;
+		numbers[1] = 5;
+		numbers[2] = 2;
+		numbers[3] = 8;
+		numbers[4] = 13;
+		numbers[5] = 6;
+		
+		int[] bnumbers = new int[6];
+		bnumbers[0] = 1;
+		bnumbers[1] = 5;
+		bnumbers[2] = 2;
+		bnumbers[3] = 8;
+		bnumbers[4] = 13;
+		bnumbers[5] = 6;
+		
+		
+		System.out.println(numbers[0]);
+		System.out.println(numbers[numbers.length - 1]);
+		
+		
+
+	}
+
+}
